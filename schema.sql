5110441c8e481dce45b4a7244b729ae39eef6100
sharkhouse23 <ocwear@gmail.com>
2018-10-26 15:04:30 -0700

    incorporated functionality to add fake add to the db

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE if exists udemy_similar_component;
+CREATE DATABASE udemy_similar_component;
+
+USE udemy_similar_component;
+
+CREATE TABLE Courses (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(75),
+  average_rating DECIMAL(2,1),
+  regular_price DECIMAL(10,2),
+  sales_price DECIMAL(10,2),
+  image_url VARCHAR(100)
+);
+
+CREATE TABLE Students (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  courses_count INT(70),
+  reviews_count INT(70)
+);
+
+CREATE TABLE Purchases (
+  course_id INTEGER(8),
+  student_id INTEGER(9)
+);
+
+ALTER TABLE Purchases ADD FOREIGN KEY (course_id) REFERENCES Courses (id);
+ALTER TABLE Purchases ADD FOREIGN KEY (student_id) REFERENCES Students (id);
