b9f4b5cb324b261d863a38f46d40321a3e21f373
sharkhouse23 <ocwear@gmail.com>
2018-11-03 11:55:00 -0700

    Incorporated CSS modules

--- client/src/components/SimilarCourses.jsx
+++ client/src/components/SimilarCourses.jsx
@@ -3,40 +3,40 @@ import styles from './styles.css';
 
 // props is object with key list = [{}, ...]
 const SimilarCourses = (props) => (
-	<div className="similar-courses-component">
-		<div className="course-recommendations">
-			<div className="course-title">Students Also Bought</div>
+	<div className={styles.similarCoursesComponent}>
+		<div className={styles.courseRecommendations}>
+			<div className={styles.courseTitle}>Students Also Bought</div>
 		  { props.courses.map(course => (<div key={course.id}>
 		  	<div>
-		  		<div className="similar-course-item-content-container">
-		  			<div className="similar-course-image-container">
-		  				<img className="course-item-img" src={course.image_url} alt={course.name}></img>
-		  				<span className="lecture-time">{course.lecture_time} hours</span>
+		  		<div className={styles.similarCourseItemContentContainer}>
+		  			<div className={styles.similarCourseImageContainer}>
+		  				<img className={styles.courseItemImg} src={course.image_url} alt={course.name}></img>
+		  				<span className={styles.lectureTime}>{course.lecture_time} hours</span>
 		  			</div>
-		  			<div className="similar-course-item-info-container">
-		  				<div className="similar-course-subtitle-container">
-		  					<div className="similar-course-name">{course.name}</div>
-		  					<div className="last-update-time">Updated {course.last_update_month}/{course.last_update_year}</div>
+		  			<div className={styles.similarCourseItemInfoContainer}>
+		  				<div className={styles.similarCourseSubtitleContainer}>
+		  					<div className={styles.similarCourseName}>{course.name}</div>
+		  					<div className={styles.lastUpdateTime}>Updated {course.last_update_month}/{course.last_update_year}</div>
 		  				</div>
-				      <div className="similar-course-other-info-container">
-				      	<span className="similar-course-rating">
-				      		<img className="star-icon-img" src="https://s3-us-west-1.amazonaws.com/fecpics/star-icon.png"></img>
+				      <div className={styles.similarCourseOtherInfoContainer}>
+				      	<span className={styles.similarCourseRating}>
+				      		<img className={styles.starIconImg} src="https://s3-us-west-1.amazonaws.com/fecpics/star-icon.png"></img>
 				      		<span className="star-rating-score">{course.average_rating}</span>
 				      	</span>
-				      	<span className="similar-course-purchase-count">
-				      		<img className="purchase-icon-img" src="https://s3-us-west-1.amazonaws.com/fecpics/purchase-order-icon.png"></img>
+				      	<span className={styles.similarCoursePurchaseCount}>
+				      		<img className={styles.PurchaseIconImg} src="https://s3-us-west-1.amazonaws.com/fecpics/purchase-order-icon.png"></img>
 				      		<span className="purchase-count">{course.purchase_count}</span>
 				      	</span>
-				      	<div className="similar-course-price-container">
-				    		  <div className="similar-course-sales-price">
-				    		  	<span className="source-only">current price</span>
+				      	<div className={styles.similarCoursePriceContainer}>
+				    		  <div className={styles.similarCourseSalesPrice}>
+				    		  	<span className={styles.sourceOnly}>current price</span>
 				    		  	<span>
 				    		  		<span>${course.sales_price}.00</span>
 				    		  	</span>
 				    		  </div>
-				    		  <div className="similar-course-reg-price">
-				    		  	<span className="source-only">original price</span>
-				    		  	<s>
+				    		  <div className={styles.similarCourseRegPrice}>
+				    		  	<span className={styles.sourceOnly}>original price</span>
+				    		  	<s className={styles.s} >
 				    		  		<span>${course.regular_price}.00</span>
 				    		  	</s>
 				    		  </div>
